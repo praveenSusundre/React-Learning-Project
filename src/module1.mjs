fbb252345dde8d7f5a23f6364fa1c0f6b98e0d88
Praveen Susundre <Praveen.Susundre@dpworld.com>
2024-11-16 22:31:30 +0530

    export default and export variables

---
+++ src/module1.mjs
@@ -0,0 +1,5 @@
+import ui,{c,a} from './module2.mjs'
+
+console.log(ui);
+console.log(c);
+console.log(a);
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+const a = "test-a";
+const b = "test-b";
+const c = "test-c";
+
+export default b;
+export {a};
+export {c};
