19f6d92d085b06421087a456594cca30e6273b8c
Praveen Susundre <Praveen.Susundre@dpworld.com>
2024-11-19 23:58:31 +0530

    v-12 : darkmode enable disable switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey'
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+    }
+  }
+
   return (
     <>
-      <Navbar title="MyWebsite" />
-      <div className="container">
-        <TextForm heading="Write Text Below"/>
+      <Navbar title="MyWebsite" mode={mode} toggleMode={toggleMode} />
+      <div>
+        <div className="container" >
+          <TextForm heading="Write Text Below" mode={mode} />
+          {/* <About/> */}
+        </div>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,8 +4,13 @@ export default function TextForm(props) {
 
     const [text, setText] = useState("praveen");
 
-    const onClickHandler = () => {
+    const onClickUpperHandler = () => {
         const textUpper = text.toUpperCase();
+        setText(textUpper);
+    }
+
+    const onClickLowerHandler = () => {
+        const textUpper = text.toLowerCase();
         setText(textUpper);
     }
 
@@ -15,11 +20,13 @@ export default function TextForm(props) {
 
     return (
         <div>
-            <h1>{props.heading}</h1>
+            <h1 style={{color: props.mode==='light'?'black':'white'}}>{props.heading}</h1>
             <div className="mb-2 mt-3">
-                <textarea className="form-control" value={text} onChange={onChangeHandler} id="exampleFormControlTextarea1" rows="6"></textarea>
+                <textarea style={{backgroundColor: props.mode==='light'?'white':'grey', color: props.mode==='light'?'black':'white'}} className="form-control" value={text} onChange={onChangeHandler} id="exampleFormControlTextarea1" rows="6"></textarea>
             </div>
-            <button type="button" onClick={onClickHandler} className="btn btn-primary">Convert to UpperCase</button>
+            <button type="button" onClick={onClickUpperHandler} className="btn btn-primary">Convert to UpperCase</button>
+            <button type="button" onClick={onClickLowerHandler} className="btn btn-primary m-2">Convert to Lowercase</button>
+            <p style={{color: props.mode==='light'?'black':'white'}} className='mt-2'>{text.split(' ').length} words and {text.length} characters</p>
         </div>
     )
 }
